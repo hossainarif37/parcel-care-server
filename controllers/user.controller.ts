e9ed1ef627cbb324d9dcb7526770c0ba86c8c13c
hossainarif37 <hossainarif4937@gmail.com>
2024-04-30 13:05:43 +0600

    feat: Add user profile retrieval by ID
    - Introduced a new route in `user.routes.ts` for fetching a user's profile by their ID.
    - Implemented `getUserProfile` controller in `user.controller.ts` to handle the request, retrieve the user's profile based on the provided ID, and return the profile information.
    - Ensured that the route is accessible to the public but requires authentication via the `checkAuth` middleware for added security.
    
    This feature enhances the user experience by allowing users to view their profile information and supports the retrieval of public user profiles by ID, enhancing the application's functionality and user interaction.

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import User from "../models/user.model";
 
 export const getCurrentUser = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -8,6 +9,21 @@ export const getCurrentUser = async (req: Request, res: Response, next: NextFunc
         })
     } catch (error) {
         console.log('Get Current User Controller: ', (error as Error).message);
+        next(error);
+    }
+}
+
+export const getUserProfile = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+        const user = await User.findById(req.params.userId, { name: 1, profilePicture: 1, _id: 1 });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' })
+        }
+        res.status(200).json({ success: true, user });
+
+    } catch (error) {
+        console.log('Get User Profile Controller: ', (error as Error).message);
         next(error);
     }
 }
--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import { getCurrentUser } from "../controllers/user.controller";
+import { getCurrentUser, getUserProfile } from "../controllers/user.controller";
 import { checkAuth } from "../middleware/authorization";
 const router = express.Router();
 
@@ -18,6 +18,21 @@ router
      * @access Private
     */
     .get('/current-user', checkAuth, getCurrentUser)
+
+    //* Get user by user ID
+    /**
+    * @route GET /api/users/:userId/profile
+    * @description Get a user's profile by ID.
+    * @access Public
+    * 
+    * @params userId - The ID of the user to retrieve the profile for.
+    * 
+    * @returns {object} - User profile information.
+    * 
+    * @throws {404} If the user is not found.
+    * @throws {500} If there's an internal server error.
+    */
+    .get('/:userId/profile', checkAuth, getUserProfile)
 
 
 
