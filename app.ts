e24865c825989236513a2c7588e572bc1c57799b
hossainarif37 <hossainarif4937@gmail.com>
2024-10-22 12:56:36 +0600

    fix: Fix the typescript error 'This kind of expression is always truthy.' in `app.ts' file

--- app.ts
+++ app.ts
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: process.env.CLIENT_URL ? [process.env.CLIENT_URL, "http://localhost:3000"] : "http://localhost:3000" || '*',
+    origin: process.env.CLIENT_URL ? [process.env.CLIENT_URL, "http://localhost:3000"] : ["http://localhost:3000"],
     methods: "GET, POST, PUT, DELETE, OPTIONS, PATCH",
     credentials: true
 }));
